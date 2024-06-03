c72c83ac48434b902595ffedb725dfdb7ecff9b1
qq815776412 <815776412@qq.com>
2024-06-03 16:29:34 +0800

    feat: use $q.cookies plugin

--- packages/theme/src/IndexPage.vue
+++ packages/theme/src/IndexPage.vue
@@ -131,35 +131,21 @@ watchEffect(() => {
 	}
 });
 
-function getCookie(name: string){
-    var strcookie = document.cookie;//获取cookie字符串
-    var arrcookie = strcookie.split("; ");//分割
-    //遍历匹配
-    for ( var i = 0; i < arrcookie.length; i++) {
-        var arr = arrcookie[i].split("=");
-        if (arr[0] == name){
-            return arr[1];
-        }
-    }
-    return "";
-}
-
-const themeValue = getCookie(themeModeName); 
+const themeValue = $q.cookies.get(themeModeName);
 if (themeValue) {
-	const theme = Number(themeValue);
+    const theme = Number(themeValue);
     if (theme == ThemeDefinedMode.AUTO) {
-	    $q.dark.set('auto');
-	} else {
-		$q.dark.set(theme == ThemeDefinedMode.DARK);
-	}
+        $q.dark.set('auto');
+    } else {
+        $q.dark.set(theme == ThemeDefinedMode.DARK);
+    }
 }
-
 </script>
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-	name: "BtTheme"
+    name: "BtTheme"
 });
 </script>
 <style></style>
