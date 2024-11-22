d09a8f0aa8127cb42b87a3fb5fc4aa5054d5541f
yyh <24493052+yongheng2016@users.noreply.github.com>
2024-11-22 17:59:02 +0800

    fix: useColor

--- packages/utils/useColor.ts
+++ packages/utils/useColor.ts
@@ -1,20 +1,22 @@
-import { useQuasar, colors, Dark } from "quasar";
-import { ref, watchEffect } from "vue";
+import { useQuasar, colors, Dark } from 'quasar';
+import { ref, watchEffect } from 'vue';
 
 const { getPaletteColor } = colors;
 
 function useColor(variable: string) {
-	const $q = useQuasar();
-	const dark = $q?.dark || Dark;
-	const color = ref(getPaletteColor(variable));
+  const $q = useQuasar();
+  const dark = $q?.dark || Dark;
+  const color = ref();
+  try {
+    color.value = getPaletteColor(variable);
 
-	watchEffect(() => {
-		color.value = dark.isActive
-			? getPaletteColor(variable)
-			: getPaletteColor(variable);
-	});
-
-	return { color };
+    watchEffect(() => {
+      color.value = dark.isActive ? getPaletteColor(variable) : getPaletteColor(variable);
+    });
+    return { color };
+  } catch (error) {
+    return { color };
+  }
 }
 
 export default useColor;
