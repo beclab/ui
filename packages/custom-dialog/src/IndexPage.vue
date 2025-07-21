4b638cdd5df72d015687b1a75d25263dab636b0e
icebergtsn <zyh2433219116@gmail.com>
2025-07-21 20:06:37 +0800

    feat: support custom dialog persistent

--- packages/custom-dialog/src/IndexPage.vue
+++ packages/custom-dialog/src/IndexPage.vue
@@ -1,90 +1,94 @@
 <template>
-  <q-dialog
-    class="card-dialog"
-    ref="dialogRef"
-    v-model="show"
-    @hide="hiddenDialog"
-    :noRouteDismiss="noRouteDismiss"
-  >
-    <q-card
-      class="card-container no-shadow column no-wrap"
-      :style="{ width, maxWidth: width, height }"
+    <q-dialog
+        class="card-dialog"
+        ref="dialogRef"
+        v-model="show"
+        :persistent="persistent"
+        @hide="hiddenDialog"
+        :noRouteDismiss="noRouteDismiss"
     >
-      <dialog-bar
-        :title="title"
-        :icon="icon"
-        :platform="platform"
-        @close="onCancel"
-      />
+        <q-card
+            class="card-container no-shadow column no-wrap"
+            :style="{ width, maxWidth: width, height }"
+        >
+            <dialog-bar
+                :title="title"
+                :icon="icon"
+                :platform="platform"
+                @close="onCancel"
+            />
 
-      <div class="dialog-content">
-        <slot />
-      </div>
+            <div class="dialog-content">
+                <slot/>
+            </div>
 
-      <dialog-footer
-        :ok="ok"
-        :cancel="cancel"
-        :okStyle="okStyle"
-        :loading="okLoading"
-        :platform="platform"
-        :skip="skip"
-        :okDisabled="okDisabled"
-        :okClass="okClass"
-        @onCancel="onCancel"
-        @onSubmit="onSubmit"
-        @onSkip="onSkip"
-      >
-        <template #footerMore>
-          <slot name="footerMore" />
-        </template>
-      </dialog-footer>
-    </q-card>
-  </q-dialog>
+            <dialog-footer
+                :ok="ok"
+                :cancel="cancel"
+                :okStyle="okStyle"
+                :loading="okLoading"
+                :platform="platform"
+                :skip="skip"
+                :okDisabled="okDisabled"
+                :okClass="okClass"
+                @onCancel="onCancel"
+                @onSubmit="onSubmit"
+                @onSkip="onSkip"
+            >
+                <template #footerMore>
+                    <slot name="footerMore"/>
+                </template>
+            </dialog-footer>
+        </q-card>
+    </q-dialog>
 </template>
 
 <script lang="ts" setup>
-import { useDialogPluginComponent } from 'quasar';
-import { ref, defineProps, computed, watch } from 'vue';
+import {useDialogPluginComponent} from 'quasar';
+import {ref, defineProps, computed, watch} from 'vue';
 
 import DialogBar from './DialogBar.vue';
 import DialogFooter from './DialogFooter.vue';
 
-import { Platform, Size } from './type';
+import {Platform, Size} from './type';
 
 interface Props {
-  platform: Platform;
-  size: Size;
-  title: string;
-  icon: string;
-  ok: string | boolean;
-  okStyle: object;
-  okClass: string;
-  cancel: string | boolean;
-  okLoading: string | boolean;
-  skip: string | boolean;
-  fullWidth: boolean;
-  fullHeight: boolean;
-  okDisabled: boolean;
-  noRouteDismiss: boolean;
-  modelValue?: boolean;
+    platform: Platform;
+    size: Size;
+    title: string;
+    icon: string;
+    persistent: boolean;
+    ok: string | boolean;
+    okStyle: object;
+    okClass: string;
+    cancel: string | boolean;
+    okLoading: string | boolean;
+    skip: string | boolean;
+    fullWidth: boolean;
+    fullHeight: boolean;
+    okDisabled: boolean;
+    noRouteDismiss: boolean;
+    modelValue?: boolean;
 }
+
 const props = withDefaults(defineProps<Props>(), {
-  platform: Platform.WEB,
-  size: Size.SMALL,
-  ok: true,
-  okStyle: () => ({}),
-  okClass: '',
-  okLoading: false,
-  fullWidth: false,
-  fullHeight: false,
-  okDisabled: false,
-  noRouteDismiss: false,
-  modelValue: true
+    platform: Platform.WEB,
+    size: Size.SMALL,
+    persistent: false,
+    ok: true,
+    okStyle: () => ({}),
+    okClass: '',
+    okLoading: false,
+    fullWidth: false,
+    fullHeight: false,
+    okDisabled: false,
+    noRouteDismiss: false,
+    modelValue: true
 });
 
 const emits = defineEmits(['onSubmit', 'onCancel', 'onSkip', 'onHide', 'update:modelValue']);
 
-const { dialogRef, onDialogCancel, onDialogOK, onDialogHide } = useDialogPluginComponent();
+const {dialogRef, onDialogCancel, onDialogOK, onDialogHide} = useDialogPluginComponent();
 
 console.log(props.modelValue)
 
@@ -98,87 +102,90 @@ const widthRatio = ref(0.86);
 const heightRatio = ref(0.75);
 
 const width = computed(() => {
-  if (props.fullWidth) {
-    const innerWidth = window.innerWidth;
-    return innerWidth * widthRatio.value + 'px';
-  }
+    if (props.fullWidth) {
+        const innerWidth = window.innerWidth;
+        return innerWidth * widthRatio.value + 'px';
+    }
 
-  switch (props.size) {
-    case Size.SMALL:
-      return '400px';
+    switch (props.size) {
+        case Size.SMALL:
+            return '400px';
 
-    case Size.MEDIUM:
-      return '560px';
+        case Size.MEDIUM:
+            return '560px';
 
-    case Size.LARGE:
-      return '800px';
+        case Size.LARGE:
+            return '800px';
 
-    default:
-      return '400px';
-  }
+        default:
+            return '400px';
+    }
 });
 
 const height = computed(() => {
-  if (props.fullHeight) {
-    const innerHeight = window.innerHeight;
-    return innerHeight * heightRatio.value + 'px';
-  }
+    if (props.fullHeight) {
+        const innerHeight = window.innerHeight;
+        return innerHeight * heightRatio.value + 'px';
+    }
 
-  return 'auto';
+    return 'auto';
 });
 
 const onSubmit = async () => {
-  emits('onSubmit');
+    emits('onSubmit');
 };
 
 let hidden = true;
 
 const onCancel = () => {
-  hidden = false;
-  emits('onCancel');
-  onDialogCancel();
+    hidden = false;
+    emits('onCancel');
+    onDialogCancel();
 };
 
 const hiddenDialog = () => {
-  if (hidden) {
-    emits('onHide');
-  }
+    if (hidden) {
+        emits('onHide');
+    }
     emits('update:modelValue', false);
 };
 
 const onSkip = () => {
-  emits('onSkip');
+    emits('onSkip');
 };
 
 defineExpose({
-  onDialogOK,
-  onDialogCancel,
-  onDialogHide
+    onDialogOK,
+    onDialogCancel,
+    onDialogHide
 });
 </script>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
+
 export default defineComponent({
-  name: 'BtCustomDialog'
+    name: 'BtCustomDialog'
 });
 </script>
 
 <style lang="scss" scoped>
 .card-dialog {
-  .card-container {
-    border-radius: 12px;
-    padding: 20px;
-    .dialog-content {
-      flex: 1;
-      margin: 20px 0 32px;
-      width: 100%;
-      max-height: calc(100vh * 0.75);
-      overflow: scroll;
-      &::-webkit-scrollbar {
-        display: none;
-      }
+    .card-container {
+        border-radius: 12px;
+        padding: 20px;
+
+        .dialog-content {
+            flex: 1;
+            margin: 20px 0 32px;
+            width: 100%;
+            max-height: calc(100vh * 0.75);
+            overflow: scroll;
+
+            &::-webkit-scrollbar {
+                display: none;
+            }
+        }
     }
-  }
 }
 </style>
