617ec5868693eed7f4e629878a620b37f7e91b40
icebergtsn <zyh2433219116@gmail.com>
2025-07-21 19:30:26 +0800

    fix: update dialog title max width

--- packages/custom-dialog/src/DialogBar.vue
+++ packages/custom-dialog/src/DialogBar.vue
@@ -1,92 +1,96 @@
 <template>
-  <div class="mobile-title" v-if="platform === 'mobile'">
-    {{ title }}
-  </div>
-
-  <div v-else class="bar text-ink-1">
-    <div class="head row items-center justify-center">
-      <q-icon v-if="icon" class="icon q-mr-sm" :name="icon" />
-      <div class="title col text-ink-1 text-left">
+    <div class="mobile-title" v-if="platform === 'mobile'">
         {{ title }}
-      </div>
     </div>
 
-    <q-btn
-      class="close-icon btn-size-md btn-no-text"
-      dense
-      flat
-      icon="sym_r_close"
-      color="ink-2"
-      v-close-popup
-      @click="close"
-    >
-      <q-tooltip>Close</q-tooltip>
-    </q-btn>
-  </div>
+    <div v-else class="bar text-ink-1">
+        <div class="head row items-center justify-between">
+            <q-icon v-if="icon" class="icon q-mr-sm" :name="icon"/>
+            <div class="title col text-ink-1 text-left">
+                {{ title }}
+            </div>
+        </div>
+
+        <q-btn
+            class="close-icon btn-size-md btn-no-text"
+            dense
+            flat
+            icon="sym_r_close"
+            color="ink-2"
+            v-close-popup
+            @click="close"
+        >
+            <q-tooltip>Close</q-tooltip>
+        </q-btn>
+    </div>
 </template>
 
 <script lang="ts" setup>
 defineProps({
-  platform: {
-    type: String,
-    default: 'web',
-    required: false
-  },
-  title: {
-    type: String,
-    default: '',
-    required: false
-  },
-  icon: {
-    type: String,
-    default: '',
-    required: false
-  }
+    platform: {
+        type: String,
+        default: 'web',
+        required: false
+    },
+    title: {
+        type: String,
+        default: '',
+        required: false
+    },
+    icon: {
+        type: String,
+        default: '',
+        required: false
+    }
 });
 
 const emits = defineEmits(['close']);
 
 const close = () => {
-  emits('close');
+    emits('close');
 };
 </script>
 
 <style scoped lang="scss">
 .mobile-title {
-  color: $ink-1;
-  font-size: 16px;
-  text-align: center;
-  font-weight: 500;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-.bar {
-  height: 24px;
-  line-height: 24px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  .head {
-    height: 24px;
-    line-height: 24px;
-  }
-
-  .icon {
-    font-size: 24px;
-  }
-
-  .title {
+    color: $ink-1;
     font-size: 16px;
-    font-weight: 700;
+    text-align: center;
+    font-weight: 500;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-  }
+}
 
-  .close-icon {
-    margin-top: -12px;
-    margin-right: -12px;
-  }
+.bar {
+    height: 24px;
+    line-height: 24px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .head {
+        height: 24px;
+        line-height: 24px;
+        max-width: calc(100% - 40px);
+
+        .icon {
+            font-size: 24px;
+        }
+
+        .title {
+            font-size: 16px;
+            font-weight: 700;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+    }
+
+    .close-icon {
+        margin-top: -12px;
+        margin-right: -12px;
+    }
 }
 </style>
